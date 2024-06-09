c482d9f279f67889d205ed9518fbdcb7dae0daa4
NoxiePC <carballosepejuan@gmail.com>
2024-06-08 22:02:29 -0300

    se configuro el servicio patients para que se registre en EurekaSV

--- PatientsAndMedicalAppointments/patients/src/main/java/patients/patients/PatientsApplication.java
+++ PatientsAndMedicalAppointments/patients/src/main/java/patients/patients/PatientsApplication.java
@@ -2,8 +2,10 @@ package patients.patients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientsApplication {
 
 	public static void main(String[] args) {
