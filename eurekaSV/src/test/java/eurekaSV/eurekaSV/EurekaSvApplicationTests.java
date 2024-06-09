169c32d6bc45785b72ab76c36694b04a1cb6641b
NoxiePC <carballosepejuan@gmail.com>
2024-06-08 21:51:22 -0300

    servidor de eureka creado y configurado

---
+++ eurekaSV/src/test/java/eurekaSV/eurekaSV/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package eurekaSV.eurekaSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
