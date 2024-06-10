007306718de8a8a64f44bb3ecc165f377579e76c
NoxiePC <carballosepejuan@gmail.com>
2024-06-09 23:58:00 -0300

    creamos el configServer

---
+++ ConfigServer/configServer/src/test/java/jics/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jics.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
